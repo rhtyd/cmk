4bb86334eaa15e3d2ce5ecfc17febb06173284cd
Rohit Yadav <rohit@yadav.cloud>
2017-12-25 15:27:27 +0530

    cmk: add main
    Signed-off-by: Rohit Yadav <rohit@yadav.cloud>

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # cmk
 
-Experimental stuff, for now please go away
+Experimental stuff, watch this space. If you find it exciting collaborate over
+email with me: rohit AT yadav.cloud
---
+++ cmk.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"./config"
+	"./interpretor"
+)
+
+func main() {
+	args := os.Args[1:]
+	cfg := config.NewConfig()
+	if len(args) > 0 {
+		interpretor.ExecCmd(cfg, args, nil, nil)
+	} else {
+		interpretor.ExecShell(cfg)
+	}
+}
